e05a8569c1b1c75ecf49915215ca487c18130eac
10KJVN <90614291+10KJVN@users.noreply.github.com>
2025-10-08 00:47:29 +0200

    Figured out instantiating at prefab pos.
    Apparently contacts isn't a thing when using OnTriggerCollision and blatantly copying the spawner logic caused double ~ triple spawns of the prefab. Googling the code snippet helped a ton though.

--- ParachutePanic/Assets/Scripts/CollisionVisualizer.cs
+++ ParachutePanic/Assets/Scripts/CollisionVisualizer.cs
@@ -1,19 +1,25 @@
+using System;
 using UnityEngine;
 
 public class CollisionVisualizer : MonoBehaviour
 {
     public GameObject prefab;
-    private void OnTriggerEnter(Collider other)
+
+    private void OnCollisionEnter(Collision other)
     {
-        GameObject go = Instantiate(prefab);
+        GameObject go = prefab;
         Debug.Log(other.gameObject.name);
+        
+        // Get the first contact point of the collision
+        ContactPoint contact = other.contacts[0];
+        
+        // Calculate rotation to align with the contact surface normal
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        
+        // Get the position of the contact point
+        Vector3 pos = contact.point;
 
-        Spawner spawner = other.gameObject.GetComponent<Spawner>();
-        if (spawner != null)
-        {
-            spawner.GetAmountSpawned();
-        }
-
-        //spawner.spawnedObjects.contacts[0].point;
+        // Instantiate the explosion prefab at the contact point
+        Instantiate(go, pos, rot);
     }
 }
