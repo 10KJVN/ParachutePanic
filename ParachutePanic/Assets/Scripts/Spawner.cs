4190de936c2806d5eb6a7db16ec93022d04c0963
10KJVN <90614291+10KJVN@users.noreply.github.com>
2025-10-07 18:25:47 +0200

    Implemented spawner from demo
    - Fixed missing material
    - Allowed both 'Active Input Handling' settings

---
+++ ParachutePanic/Assets/Scripts/Spawner.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    private List<GameObject> spawnedObjects;
+
+    private void Start()
+    {
+        if (prefab == null)
+            Debug.LogWarning("Prefab was null");
+
+        spawnedObjects = new List<GameObject>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Spawn();
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Despawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        GameObject go = Instantiate(prefab);
+
+        float x = Random.Range(-10, 10);
+        float y = Random.Range(-10, 10);
+        float z = Random.Range(-10, 10);
+
+        go.transform.localPosition = new Vector3(x, y, z);
+
+        Rigidbody rb = go.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+
+        spawnedObjects.Add(go);
+    }
+
+    private void Despawn()
+    {
+        if (spawnedObjects.Count <= 0)
+            return;
+
+        int i = Random.Range(0, spawnedObjects.Count);
+
+        Destroy(spawnedObjects[i]);
+        spawnedObjects.RemoveAt(i);
+    }
+
+    public int GetAmountSpawned()
+    {
+        return spawnedObjects.Count;
+    }
+}
