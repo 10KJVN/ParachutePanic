254ed811dee65c5f9eba9829e2264eeb73b0799f
10KJVN <90614291+10KJVN@users.noreply.github.com>
2025-10-21 15:04:31 +0200

    Adding force to X of rigidbody in Update to test.

--- ParachutePanic/Assets/Scripts/Cluster.cs
+++ ParachutePanic/Assets/Scripts/Cluster.cs
@@ -6,4 +6,16 @@ public class Cluster : MonoBehaviour
 {
     public Rigidbody2D rb;
     
+    [Header("Math variables")] // Default values
+    [SerializeField] private float additiveForce = 1f;
+
+    private void Start()
+    {
+        //throw new NotImplementedException();
+    }
+
+    private void Update()
+    {
+        rb.AddForceX(additiveForce * Time.deltaTime);
+    }
 }
