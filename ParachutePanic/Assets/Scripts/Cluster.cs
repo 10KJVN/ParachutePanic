b0bf5ac93ce6b73bed678144ee4a2277edf6bf89
10KJVN <90614291+10KJVN@users.noreply.github.com>
2025-10-21 15:54:20 +0200

    Attempt one at inversing

--- ParachutePanic/Assets/Scripts/Cluster.cs
+++ ParachutePanic/Assets/Scripts/Cluster.cs
@@ -5,24 +5,30 @@ using Random = UnityEngine.Random;
 public class Cluster : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public SpriteRenderer sr;
     
     [Header("Math variables")] // Default values
     [SerializeField] private float additiveForce = 1.0f;
-
+    [SerializeField] private float downForce = 1.0f;
+   
     private float rotation = 0.0f;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
     
     private void FixedUpdate()
     {
         rb.AddForceX(additiveForce * Time.deltaTime);
+        rb.AddForceY(-downForce * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         additiveForce = -additiveForce; // Flips move direction
+        //rb.rotation = -180;
+        sr.flipX = true;
     }
 }
