0943d145efe1dde02d40a5c3b6e58a1fd5f55398
10KJVN <90614291+10KJVN@users.noreply.github.com>
2025-10-31 11:33:20 +0100

    Pausing now auto selects the resume button.
    Also communicates the selected button better in the UI for controller support and clarity.

--- ParachutePanic/Assets/Scripts/Menu.cs
+++ ParachutePanic/Assets/Scripts/Menu.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Menu : MonoBehaviour
 {
-    public GameObject pauseFirstButton, optionsFirstButton, optionsClosedButton;
+    public GameObject pauseMenu, optionsMenu;
+    public GameObject pauseFirstButton, optionsFirstButton, optionsSecondButton;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetButtonDown("Fire3"))
+        {
+            PauseUnpause();
+        }
+    }
+
+    public void PauseUnpause()
+    {
+        if (!pauseMenu.activeInHierarchy)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            
+            // Clear selected object
+            EventSystem.current.SetSelectedGameObject(null);
+            // Set a new selected object
+            EventSystem.current.SetSelectedGameObject(pauseFirstButton);
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            //optionsMenu.SetActive(false);
+        }
     }
 }
