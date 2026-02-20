da019004e15b07e7ba49c469f9a51437518a7300
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-02-20 16:28:59 +0100

    Refactored Cluster.cs according to conventions.
    A little bit of a nuisance down in the collision score logic, but i'll fix sometime else.

--- ParachutePanic/Assets/Scripts/Cluster/Cluster.cs
+++ ParachutePanic/Assets/Scripts/Cluster/Cluster.cs
@@ -1,6 +1,4 @@
-using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 /// <summary>
@@ -12,91 +10,73 @@ using Random = UnityEngine.Random;
 
 public class Cluster : MonoBehaviour
 {
-    [Header("Movement Variable")]
+    [Header( "Movement Variable" )]
     public float bounceSpeed;
     
     [SerializeField] private ScoreManager scoreManager;
-    
-    // Renamed for clarity based off Peer review feedback./
+
     private float horizontalMoveSpeed;
     private float verticalMoveSpeed;
     private int posX;
     private int posY;
     private int speedX;
     private int speedY;
-    
-    // Custom init function
-    // Pass begin params
 
-    // The awake serves as a Constructor
-    private void Awake()
-    {   
-        // This function gives random starting parametrs
-        // To each cluster initiated to avoid them from being bland 'n the same.
-        //InitiateCluster();
-    }
-
-    private void InitiateCluster(int height)
+    private void InitiateCluster( int height )
     {
-        posX = Random.Range(-9, 9);
+        posX = Random.Range( -9, 9 );
         posY = height * -5;
         
-        speedX = Random.Range(7, 16);
-        speedY = Random.Range(6, 7);
-        
-        //Debug.Log("Position + " + posX + "Height: " + posY);
-        //Debug.Log("Speed: " + speedX + speedY);
+        speedX = Random.Range( 7, 16 );
+        speedY = Random.Range( 6, 7 );
     }
 
 
     private void Start()
     {
-        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+        scoreManager = GameObject.Find( "ScoreManager" ).GetComponent<ScoreManager>();
         horizontalMoveSpeed = posX + speedX;
         verticalMoveSpeed = posY + speedY;
         
-        for (int t = 0; t <= 10; t += 1)
+        for ( int t = 0; t <= 10; t += 1 )
         {
-            InitiateCluster(t);
+            InitiateCluster( t );
         }
     }
 
     private void Update()
     {
-        //UpdateClusterPosition();
-        transform.Translate(Vector2.right * horizontalMoveSpeed * Time.deltaTime);
-        transform.Translate(Vector2.down * verticalMoveSpeed * Time.deltaTime);
+        transform.Translate( Vector2.right * horizontalMoveSpeed * Time.deltaTime );
+        transform.Translate( Vector2.down * verticalMoveSpeed * Time.deltaTime );
         UpdateClusterPosition();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D( Collider2D collision )
     {
-        // This is basically the 'CalculateVelocity' function.
-        if (collision.gameObject.CompareTag("Boundary"))
+        if (collision.gameObject.CompareTag( "Boundary" ))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+            transform.position = new Vector3( transform.position.x, transform.position.y - 1, transform.position.z );
             horizontalMoveSpeed *= -1;
         }
         
-        if (collision.gameObject.CompareTag("BoundsOut"))
+        if (collision.gameObject.CompareTag( "BoundsOut" ))
         {
-            // Destroys whatever goes out of bounds.
-            scoreManager.ChangeScore(-1);
-            Destroy(gameObject);
+            scoreManager.ChangeScore( -1 );
+            Destroy( gameObject );
         }
         
-        // I want If Enemy missed player, score +2
-        // Don't know how so -> BottomBound.cs
+        // If Enemy missed player, score +2
+        // Score Calculation -> BottomBound.cs
     }
 
-    int CalculatePosition(int currentPos, int velocity)
+    int CalculatePosition( int currentPos, int velocity )
     {
         return currentPos + velocity;
     }
 
-    int CalculateVelocity(int pos, int velocity, int min, int max)
+    int CalculateVelocity( int pos, int velocity, int min, int max )
     {
-        if (pos <= min || pos >= max)
+        if ( pos <= min || pos >= max )
         {
             velocity *= -1;
         }
@@ -106,13 +86,11 @@ public class Cluster : MonoBehaviour
 
     private void UpdateClusterPosition()
     {
-        posX = CalculatePosition(posX, speedX);
-        speedX = CalculateVelocity(posX, speedX, -10, 10);
+        posX = CalculatePosition( posX, speedX );
+        speedX = CalculateVelocity( posX, speedX, -10, 10 );
         
-        posY = CalculatePosition(posY, speedY);
-        //speedY = CalculateVelocity(posY, speedY, -10, 10);
+        posY = CalculatePosition( posY, speedY );
 
-        // 6 bounceSpeed is overkill, recommend to keep under 1.
         horizontalMoveSpeed += posX * bounceSpeed * Time.deltaTime;
         verticalMoveSpeed = posY * Time.deltaTime / 2;
     }
