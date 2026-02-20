bb54f7a841465a6f22281da6e84e6046d30b3333
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-02-20 16:31:46 +0100

    Added todo in summary

--- ParachutePanic/Assets/Scripts/Cluster/Cluster.cs
+++ ParachutePanic/Assets/Scripts/Cluster/Cluster.cs
@@ -6,6 +6,8 @@ using Random = UnityEngine.Random;
 /// Parachutes, Obstacles, Heals you name it.
 /// It initiates these with randomized speed values as well.
 /// BounceSpeed is the multiplying factor of the HorizontalMoveSpeed.
+/// 
+/// TO-DO: Less Magic numbers in InitCluster function.
 /// </summary>
 
 public class Cluster : MonoBehaviour
