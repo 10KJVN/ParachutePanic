9a8e431da16b729a3c26d8fcc36f2d445a171a3d
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-03-10 15:02:43 +0100

    Initial set up of InputHandler & Entries

---
+++ ParachutePanic/Assets/Scripts/InputEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class InputEntry
+{
+    public string playerName;
+    public int points;
+
+    // ctor
+    public InputEntry (string name, int points)
+    {
+        playerName = name;
+        this.points = points;
+    }
+}
---
+++ ParachutePanic/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] InputField nameInput;
+
+    List<InputEntry> entries = new List<InputEntry>();
+
+    public void AddNameToList()
+    {
+        // Create a new object and set name and points via the ctor.
+        entries.Add(new InputEntry(nameInput.text, Random.Range(0, 100)));
+        nameInput.text = "";
+    }
+}
