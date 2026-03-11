12d03e33cf441305f889d2e824f307676d51c34f
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-03-11 22:33:21 +0100

    Interim commit on new Highscore classes
    Currently i've kinda coded myself into a spaghetti of multiple classes and responsibilities that are to be replaced. my ScoreManager.cs used to manage both regular scores and highscores but im in the middle of adapting a new method before i refactor the old metholodigies out.

---
+++ ParachutePanic/Assets/Scripts/UI/HighscoreUI.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class HighscoreUI : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    [SerializeField] GameObject highscoreUIElementPrefab;
+    [SerializeField] Transform elementWrapper;
+
+    List<GameObject> uiElements = new List<GameObject>();
+
+    public void ShowPanel()
+    {
+        panel.SetActive(true);
+    }
+
+    public void ClosePanel()
+    {
+        panel.SetActive(false);
+    }
+
+    private void UpdateUI(List<HighscoreElement> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            HighscoreElement element = list[i];
+
+            if (element.points > 0)
+            {
+                if (i >= uiElements.Count)
+                {
+                    // instantiate new entry
+                    var inst = Instantiate(highscoreUIElementPrefab, Vector3.zero, Quaternion.identity);
+                    inst.transform.SetParent(elementWrapper, false);
+
+                    uiElements.Add(inst);
+                }
+
+                // write or overwrite name & points
+                var texts = uiElements[i].GetComponentsInChildren<Text>();
+                texts[0].text = element.playerName;
+                texts[1].text = element.points.ToString();
+            }
+        }
+
+    }
+}
